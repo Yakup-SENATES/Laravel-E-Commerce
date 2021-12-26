8e255287a0175adfbc06ddf01d5b3c6c1874f783
Yakup SENATES <76456346+Yakup-SENATES@users.noreply.github.com>
2021-12-26 22:24:32 +0300

    Display Data on User Dashboard

--- app/Http/Livewire/User/UserDashboardComponent.php
+++ app/Http/Livewire/User/UserDashboardComponent.php
@@ -2,12 +2,19 @@
 
 namespace App\Http\Livewire\User;
 
+use App\Models\Order;
 use Livewire\Component;
 
 class UserDashboardComponent extends Component
 {
     public function render()
     {
-        return view('livewire.user.user-dashboard-component')->layout('layouts.base');
+        $orders = Order::orderBy('created_at', 'desc')->where('user_id', auth()->user()->id)->get()->take(10);
+        $totalCost = Order::where('status', '!=', 'canceled')->where('user_id', auth()->user()->id)->sum('total');
+        $totalPurchase = Order::where('status', '!=', 'canceled')->where('user_id', auth()->user()->id)->count();
+        $totalDelivered = Order::where('status', 'delivered')->where('user_id', auth()->user()->id)->count();
+        $totalCanceled = Order::where('status', 'canceled')->where('user_id', auth()->user()->id)->count();
+
+        return view('livewire.user.user-dashboard-component', compact('orders', 'totalCost', 'totalPurchase', 'totalDelivered', 'totalCanceled'))->layout('layouts.base');
     }
 }
